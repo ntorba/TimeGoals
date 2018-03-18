f69db6c160505e0811c47069db09f1ca746a58ac
Nicholas Torba <nicholastorba@Nicholass-MacBook-Pro-2.local>
2018-03-18 12:32:32 -0400

    Second commit

--- TimeGoals/collection/admin.py
+++ TimeGoals/collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+#import my models
+from collection.models import Profile
+
+
+#set up atuomated slug creation
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('name', 'description',)
+    prepopulated_fields={'slug':('name',)}
+
+#register model
+admin.site.register(Profile, ProfileAdmin)
